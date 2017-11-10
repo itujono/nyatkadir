7beb79d6e2a29dad9dff3eb72d5b257d8e6538c4
itujono <itujono@gmail.com>
2017-11-10 17:39:59 +0700

    Boleh lah voting nya.

--- application/views/templates/frontend/home.php
+++ application/views/templates/frontend/home.php
@@ -215,11 +215,44 @@
     </div>
 </section>
 
-<section class="hero is-medium is-success voting">
+<section class="hero is-medium voting">
     <div class="hero-body">
         <div class="container">
-            <h3 class="title">Bakal naro module Voting di sini</h3>
-            <h4 class="subtitle">Tapi masih bingung user interface nya</h4>
+            <div class="columns">
+                <div class="column is-one-third">
+                    <div class="section-title dark">
+                        <h3 class="mb10">Mau ikut <span class="yellowish">berpartisipasi</span> <br> dalam pemberian opini?</h3>
+                        Hari di mana demokrasi merupakan hak-hak yang mutlak bagi seluruh rakyat. Semua suku bangsa yang ada di Indonesia, haruslah bersatu tanpa terkecuali.
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="content">
+                        <h3 class="title">Bagaimana pendapatmu tentang kejadian Setya Novanto di Rumah Sakit Jakarta?</h3>
+                        <div class="field">
+                            <div class="control">
+                                <label class="radio">
+                                  <input type="radio" name="question">
+                                  Biasa aja
+                                </label>
+                                <label class="radio">
+                                  <input type="radio" name="question">
+                                  Nggak bisa lah
+                                </label>
+                                <label class="radio">
+                                  <input type="radio" name="question">
+                                  Ya bisa tapi nggak boleh
+                                </label>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <div class="control">
+                                <button class="button is-primary">Submit</button>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div> <!-- kelar Column Radio -->
+            </div> <!-- kelar div Columns -->
         </div>
     </div>
 </section>
