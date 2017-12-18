cf40103aa70da361f4b94798f4ad92060d066e44
itujono <itujono@gmail.com>
2017-12-18 10:48:23 +0700

    Betulin tampilan Gallery.

--- application/views/templates/frontend/gallery.php
+++ application/views/templates/frontend/gallery.php
@@ -11,51 +11,53 @@
 
 <section class="few-gallery section">
 
-    <section class="image-gallery section">
-        <div class="columns">
-            <div class="column is-one-third">
-                <div class="section-title dark">
-                    <h3 class="mb10">Galeri <span class="yellowish">Foto</span></h3>
-                    Hari di mana demokrasi merupakan hak-hak yang mutlak bagi seluruh rakyat. Semua suku bangsa yang ada di Indonesia, haruslah bersatu tanpa terkecuali.
-                </div>
-            </div>
-            <div class="column">
-                <div class="gallery-wrapper">
-                <?php
-                    if(!empty($listgallery)){
-                        $i= 0;
-                        foreach ($listgallery as $key => $gal) {
-                        if($gal->categoryGALLERY == 1){
-                        $i++;
-                        if($i == 1){
-                ?>
-                    <div class="tile is-ancestor">
-                        <?php } ?>
-                        <div class="tile is-parent">
-                            <div class="tile is-child box">
-                                <a href="<?php echo $gal->imageGALLERY;?>" data-fancybox="image" data-caption="<?php echo $gal->titleGALLERY;?>">
-                                    <div class="image">
-                                        <img src="<?php echo $gal->imageGALLERY;?>" alt="<?php echo $gal->titleGALLERY;?>">
-                                    </div>
-                                    <h4><?php echo $gal->titleGALLERY;?></h4>
-                                </a>
-                            </div>
-                        </div>
-                    <?php
-                    if($i == 4){
-                    $i = 0;
-                    ?>
+    <div class="container">
+        <section class="image-gallery section">
+            <div class="columns">
+                <div class="column is-one-third">
+                    <div class="section-title dark">
+                        <span class="bar"></span>
+                        <h3 class="mb10">Galeri <span class="yellowish">Foto</span></h3>
+                        Hari di mana demokrasi merupakan hak-hak yang mutlak bagi seluruh rakyat. Semua suku bangsa yang ada di Indonesia, haruslah bersatu tanpa terkecuali.
                     </div>
+                </div>
+                <div class="column">
+                    <div class="gallery-wrapper">
+                    <?php
+                        if(!empty($listgallery)){
+                            $i= 0;
+                            foreach ($listgallery as $key => $gal) {
+                            if($gal->categoryGALLERY == 1){
+                            $i++;
+                            if($i == 1){
+                    ?>
+                        <div class="tile is-ancestor">
+                            <?php } ?>
+                            <div class="tile is-parent">
+                                <div class="tile is-child box">
+                                    <a href="<?php echo $gal->imageGALLERY;?>" data-fancybox="image" data-caption="<?php echo $gal->titleGALLERY;?>">
+                                        <div class="image">
+                                            <img src="<?php echo $gal->imageGALLERY;?>" alt="<?php echo $gal->titleGALLERY;?>">
+                                        </div>
+                                        <h4><?php echo $gal->titleGALLERY;?></h4>
+                                    </a>
+                                </div>
+                            </div>
+                        <?php
+                        if($i == 4){
+                        $i = 0;
+                        ?>
+                        </div>
+                                    <?php }?>
                                 <?php }?>
                             <?php }?>
                         <?php }?>
-                    <?php }?>
-                </div> <!-- kelar Gallery-Wrapper / Ancestor tile -->
-            </div>
-        </div> <!-- kelar div Columns -->
-    </section> <!-- kelar Section Gallery-Image -->
+                    </div> <!-- kelar Gallery-Wrapper / Ancestor tile -->
+                </div>
+            </div> <!-- kelar div Columns -->
+        </section> <!-- kelar Section Gallery-Image -->
 
-    <section class="video-gallery section">
+        <section class="video-gallery section">
         <div class="columns">
             <div class="column is-one-third">
                 <div class="section-title dark">
@@ -100,5 +102,5 @@
             </div>
         </div> <!-- kelar div Columns -->
     </section> <!-- kelar Section Gallery-Video -->
-
+    </div>
 </section>
