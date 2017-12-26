47b6da954cac8c3d8b84cc872f70c4e27b1d6530
itujono <itujono@gmail.com>
2017-12-26 17:30:48 +0700

    Article page diupdate!

--- application/views/templates/frontend/article.php
+++ application/views/templates/frontend/article.php
@@ -15,132 +15,55 @@
             <h3 class="mb10">Buah Pikiran <span class="reddish">Nyat Kadir</span> Untuk Direnungkan</h3>
             <p>Dan bagaimana pemikiran-pemikiran Nyat Kadir amat menarik untuk disimak.</p>
         </div>
-        <div class="tile is-ancestor">
-            <div class="tile is-parent is-6">
-                <div class="tile is-child box">
-                    <a href="<?php echo base_url();?>article/detail_article" class="title">Sesungguhnya Dunia itu Fana Adanya</a>
-                    <p class="subtitle">21 Agustus 2017</p>
-                    <a href="<?php echo base_url();?>article/detail_article" class="link">
-                        <span class="icon mdi mdi-chevron-right"></span>
-                    </a>
-                </div>
+        <div class="article-wrapper">
+            <div class="tile box" style="background-image: url(<?php echo base_url().$this->data['asfront'];?>img/nyatkadir.jpg);">
+                <a href="<?php echo base_url();?>article/detail_article" class="title">Tak Ada Manusia yang Berjiwa Abadi di Surga</a>
+                <p class="subtitle">21 Agustus 2017</p>
+                <a href="<?php echo base_url();?>article/detail_article" class="link">
+                    <span class="icon mdi mdi-chevron-right"></span>
+                </a>
             </div>
-            <div class="tile is-parent has-bg">
-                <div class="tile is-child box" style="background: url(<?php echo base_url().$this->data['asfront'];?>img/nyatkadir.jpg);">
-                    <a href="<?php echo base_url();?>article/detail_article" class="title">Tak Ada Manusia yang Berjiwa Abadi di Surga</a>
-                    <p class="subtitle">21 Agustus 2017</p>
-                    <a href="<?php echo base_url();?>article/detail_article" class="link">
-                        <span class="icon mdi mdi-chevron-right"></span>
-                    </a>
-                </div>
+            <div class="tile box" style="background-image: url(<?php echo base_url().$this->data['asfront'];?>img/campaign.jpg);">
+                <a href="<?php echo base_url();?>article/detail_article" class="title">Membuat Kulit Wol Dari Jerami Basah</a>
+                <p class="subtitle">21 Agustus 2017</p>
+                <a href="<?php echo base_url();?>article/detail_article" class="link">
+                    <span class="icon mdi mdi-chevron-right"></span>
+                </a>
             </div>
-            <div class="tile is-parent">
-                <div class="tile is-child box">
-                    <a href="<?php echo base_url();?>article/detail_article" class="title">Apakah Anda Pikir Anda Adalah Penentu Keluarga?</a>
-                    <p class="subtitle">21 Agustus 2017</p>
-                    <a href="<?php echo base_url();?>article/detail_article" class="link">
-                        <span class="icon mdi mdi-chevron-right"></span>
-                    </a>
-                </div>
+            <div class="tile box" style="background-image: url(<?php echo base_url().$this->data['asfront'];?>img/nyatkadir.jpg);">
+                <a href="<?php echo base_url();?>article/detail_article" class="title">Tak Ada Manusia yang Berjiwa Abadi di Surga</a>
+                <p class="subtitle">21 Agustus 2017</p>
+                <a href="<?php echo base_url();?>article/detail_article" class="link">
+                    <span class="icon mdi mdi-chevron-right"></span>
+                </a>
             </div>
-        </div>
-
-        <div class="tile is-ancestor">
-            <div class="tile is-parent has-bg">
-                <div class="tile is-child box" style="background: url(<?php echo base_url().$this->data['asfront'];?>img/campaign.jpg);">
-                    <a href="<?php echo base_url();?>article/detail_article" class="title">Membuat Kulit Wol Dari Jerami Basah</a>
-                    <p class="subtitle">21 Agustus 2017</p>
-                    <a href="<?php echo base_url();?>article/detail_article" class="link">
-                        <span class="icon mdi mdi-chevron-right"></span>
-                    </a>
-                </div>
+            <div class="tile box" style="background-image: url(<?php echo base_url().$this->data['asfront'];?>img/nyatkadir1.jpg);">
+                <a href="<?php echo base_url();?>article/detail_article" class="title">Apakah Anda Pikir Anda Adalah Penentu Keluarga?</a>
+                <p class="subtitle">21 Agustus 2017</p>
+                <a href="<?php echo base_url();?>article/detail_article" class="link">
+                    <span class="icon mdi mdi-chevron-right"></span>
+                </a>
             </div>
-            <div class="tile is-parent">
-                <div class="tile is-child box">
-                    <a href="<?php echo base_url();?>article/detail_article" class="title">Tak Ada Manusia yang Berjiwa Abadi di Surga</a>
-                    <p class="subtitle">21 Agustus 2017</p>
-                    <a href="<?php echo base_url();?>article/detail_article" class="link">
-                        <span class="icon mdi mdi-chevron-right"></span>
-                    </a>
-                </div>
+            <div class="tile box" style="background-image: url(<?php echo base_url().$this->data['asfront'];?>img/nyatkadir.jpg);">
+                <a href="<?php echo base_url();?>article/detail_article" class="title">Tak Ada Manusia yang Berjiwa Abadi di Surga</a>
+                <p class="subtitle">21 Agustus 2017</p>
+                <a href="<?php echo base_url();?>article/detail_article" class="link">
+                    <span class="icon mdi mdi-chevron-right"></span>
+                </a>
             </div>
-            <div class="tile is-parent is-6">
-                <div class="tile is-child box">
-                    <a href="<?php echo base_url();?>article/detail_article" class="title">Apakah Anda Pikir Anda Adalah Penentu Keluarga?</a>
-                    <p class="subtitle">21 Agustus 2017</p>
-                    <a href="<?php echo base_url();?>article/detail_article" class="link">
-                        <span class="icon mdi mdi-chevron-right"></span>
-                    </a>
-                </div>
+            <div class="tile box" style="background-image: url(<?php echo base_url().$this->data['asfront'];?>img/campaign.jpg);">
+                <a href="<?php echo base_url();?>article/detail_article" class="title">Membuat Kulit Wol Dari Jerami Basah</a>
+                <p class="subtitle">21 Agustus 2017</p>
+                <a href="<?php echo base_url();?>article/detail_article" class="link">
+                    <span class="icon mdi mdi-chevron-right"></span>
+                </a>
             </div>
-        </div>
-
-        <div class="tile is-ancestor">
-            <div class="tile is-parent">
-                <div class="tile is-child box">
-                    <a href="<?php echo base_url();?>article/detail_article" class="title">Sesungguhnya Dunia itu Fana Adanya</a>
-                    <p class="subtitle">21 Agustus 2017</p>
-                    <a href="<?php echo base_url();?>article/detail_article" class="link">
-                        <span class="icon mdi mdi-chevron-right"></span>
-                    </a>
-                </div>
-            </div>
-            <div class="tile is-parent is-6 has-bg">
-                <div class="tile is-child box" style="background: url(<?php echo base_url().$this->data['asfront'];?>img/nyatkadir.jpg);">
-                    <a href="<?php echo base_url();?>article/detail_article" class="title">Tak Ada Manusia yang Berjiwa Abadi di Surga</a>
-                    <p class="subtitle">21 Agustus 2017</p>
-                    <a href="<?php echo base_url();?>article/detail_article" class="link">
-                        <span class="icon mdi mdi-chevron-right"></span>
-                    </a>
-                </div>
-            </div>
-            <div class="tile is-parent">
-                <div class="tile is-child box">
-                    <a href="<?php echo base_url();?>article/detail_article" class="title">Apakah Anda Pikir Anda Adalah Penentu Keluarga?</a>
-                    <p class="subtitle">21 Agustus 2017</p>
-                    <a href="<?php echo base_url();?>article/detail_article" class="link">
-                        <span class="icon mdi mdi-chevron-right"></span>
-                    </a>
-                </div>
-            </div>
-        </div>
-
-        <div class="tile is-ancestor">
-            <div class="tile is-parent">
-                <div class="tile is-child box">
-                    <a href="<?php echo base_url();?>article/detail_article" class="title">Sesungguhnya Dunia itu Fana Adanya</a>
-                    <p class="subtitle">21 Agustus 2017</p>
-                    <a href="<?php echo base_url();?>article/detail_article" class="link">
-                        <span class="icon mdi mdi-chevron-right"></span>
-                    </a>
-                </div>
-            </div>
-            <div class="tile is-parent">
-                <div class="tile is-child box">
-                    <a href="<?php echo base_url();?>article/detail_article" class="title">Tak Ada Manusia yang Berjiwa Abadi di Surga</a>
-                    <p class="subtitle">21 Agustus 2017</p>
-                    <a href="<?php echo base_url();?>article/detail_article" class="link">
-                        <span class="icon mdi mdi-chevron-right"></span>
-                    </a>
-                </div>
-            </div>
-            <div class="tile is-parent">
-                <div class="tile is-child box">
-                    <a href="<?php echo base_url();?>article/detail_article" class="title">Apakah Anda Pikir Anda Adalah Penentu Keluarga?</a>
-                    <p class="subtitle">21 Agustus 2017</p>
-                    <a href="<?php echo base_url();?>article/detail_article" class="link">
-                        <span class="icon mdi mdi-chevron-right"></span>
-                    </a>
-                </div>
-            </div>
-            <div class="tile is-parent has-bg">
-                <div class="tile is-child box" style="background: url(<?php echo base_url().$this->data['asfront'];?>img/nyatkadir1.jpg);">
-                    <a href="<?php echo base_url();?>article/detail_article" class="title">Apakah Anda Pikir Anda Adalah Penentu Keluarga?</a>
-                    <p class="subtitle">21 Agustus 2017</p>
-                    <a href="<?php echo base_url();?>article/detail_article" class="link">
-                        <span class="icon mdi mdi-chevron-right"></span>
-                    </a>
-                </div>
+            <div class="tile box" style="background-image: url(<?php echo base_url().$this->data['asfront'];?>img/nyatkadir.jpg);">
+                <a href="<?php echo base_url();?>article/detail_article" class="title">Tak Ada Manusia yang Berjiwa Abadi di Surga</a>
+                <p class="subtitle">21 Agustus 2017</p>
+                <a href="<?php echo base_url();?>article/detail_article" class="link">
+                    <span class="icon mdi mdi-chevron-right"></span>
+                </a>
             </div>
         </div>
     </div>
