b709e99bc03876c4847106bf38ed745a1a8aa2d0
itujono <itujono@gmail.com>
2017-12-30 01:15:25 +0700

    Oke sip static gallery di VIdeo nya.

--- application/views/templates/frontend/gallery.php
+++ application/views/templates/frontend/gallery.php
@@ -347,79 +347,209 @@
                     Hari di mana demokrasi merupakan hak-hak yang mutlak bagi seluruh rakyat. Semua suku bangsa yang ada di Indonesia, haruslah bersatu tanpa terkecuali.
                 </div>
                 <div class="ui styled accordion">
-                    <?php
-                    if(!empty($list_month_gallery)){
-                        foreach ($list_month_gallery as $key => $month) {
-                            if($month->categoryGALLERY == 2){
-                            if($key == 0){
-                                $active = 'active';
-                                $div = 'ui menu content active';
-                            } else {
-                                $active = '';
-                                $div = 'content';
-                            }
-                        $date_form = dF($month->createdateGALLERY, "m");
-                    ?>
-                    <div class="title <?php echo $active;?>">
+
+
+
+                    <!-- Awak comment dulu ya. Nak awak inspect pake versi static dulu  -->
+                    <!-- <?php
+                    // if(!empty($list_month_gallery)){
+                    //     foreach ($list_month_gallery as $key => $month) {
+                    //         if($month->categoryGALLERY == 2){
+                    //         if($key == 0){
+                    //             $active = 'active';
+                    //             $div = 'ui menu content active';
+                    //         } else {
+                    //             $active = '';
+                    //             $div = 'content';
+                    //         }
+                    //     $date_form = dF($month->createdateGALLERY, "m");
+                    // ?>
+                    <div class="title <?php //echo $active;?>">
                         <i class="dropdown icon"></i>
-                        <?php echo indonesian_date($month->createdateGALLERY,'F Y',''); ?>
+                        <?php //echo indonesian_date($month->createdateGALLERY,'F Y',''); ?>
                     </div>
-                    <div class="<?php echo $div;?>">
+                    <div class="<?php //echo $div;?>">
                         <ul>
                         <?php
-                        foreach ($listgallery as $keys => $gal) {
-                            if($gal->categoryGALLERY == 2){
-                            if($keys == 0){
-                                $active = 'active';
-                            } else {
-                                $active = '';
-                            }
-                            $date_format_gallery = dF($gal->createdateGALLERY, "m");
-                                if($date_form == $date_format_gallery){
-                        ?>
-                            <li><a href="#" class="item <?php echo $active;?>" data-tab="<?php echo replacesymbol_tounderscore($gal->titleGALLERY); ?>"><?php echo $gal->titleGALLERY; ?></a></li>
-                                <?php } ?>
-                            <?php } ?>
-                        <?php } ?>
+                        // foreach ($listgallery as $keys => $gal) {
+                        //     if($gal->categoryGALLERY == 2){
+                        //     if($keys == 0){
+                        //         $active = 'active';
+                        //     } else {
+                        //         $active = '';
+                        //     }
+                        //     $date_format_gallery = dF($gal->createdateGALLERY, "m");
+                        //         if($date_form == $date_format_gallery){
+                        // ?>
+                            <li><a href="#" class="item <?php //echo $active;?>" data-tab="<?php //echo replacesymbol_tounderscore($gal->titleGALLERY); ?>"><?php //echo $gal->titleGALLERY; ?></a></li>
+                                <?php //} ?>
+                            <?php //} ?>
+                        <?php //} ?>
                         </ul>
                     </div>
-                            <?php } ?>
-                        <?php } ?>
-                    <?php } ?>
+                            <?php //} ?>
+                        <?php //} ?>
+                    <?php //} ?> -->
+
+
+                    <div class="title">
+                        <i class="dropdown icon"></i>
+                        November 2017
+                    </div>
+                    <div class="ui menu content">
+                        <ul>
+                            <li>
+                                <a href="#" class="item" data-tab="nyat_kadir_gelar_buka_bersama_dengan_anak_jalanan">Nyat Kadir gelar buka bersama dengan anak jalanan</a>
+                            </li>
+                            <li>
+                                <a href="#" class="item" data-tab="besar_besaran_kata">Besar-besaran Kata</a>
+                            </li>
+                        </ul>
+                    </div>
+
+
                 </div> <!-- kelar Accordion -->
             </div>
             <div class="column">
                 <div class="gallery-wrapper">
-                    <?php
-                    foreach ($listgallery as $keying => $gallery) {
-                        if($gallery->categoryGALLERY == 2){
-                        $thumbnail = get_thumbnail_from_youtube($gallery->linkvideoGALLERY);
-                        if($keying == 0){
-                            $active = 'active';
-                        } else {
-                            $active = '';
-                        }
+
+
+
+                    <!-- Dinamisnya awak comment dulu ya wak. Awak coba inspect di static nya dulu -->
+                    <!-- <?php
+                    // foreach ($listgallery as $keying => $gallery) {
+                    //     if($gallery->categoryGALLERY == 2){
+                    //     $thumbnail = get_thumbnail_from_youtube($gallery->linkvideoGALLERY);
+                    //     if($keying == 0){
+                    //         $active = 'active';
+                    //     } else {
+                    //         $active = '';
+                    //     }
                     ?>
-                    <div class="ui tab hero is-primary <?php echo $active;?>" data-tab="<?php echo strtolower(replacesymbol_tounderscore($gallery->titleGALLERY)); ?>">
+                    <div class="ui tab hero is-primary <?php //echo $active;?>" data-tab="<?php //echo strtolower(replacesymbol_tounderscore($gallery->titleGALLERY)); ?>">
                         <div class="hero-body">
-                            <h1 class="title"><?php echo $gallery->titleGALLERY; ?></h1>
+                            <h1 class="title"><?php //echo $gallery->titleGALLERY; ?></h1>
                             <div class="tile is-ancestor">
                                 <div class="tile is-parent">
                                     <div class="tile is-child box">
-                                        <a href="<?php echo $gallery->linkvideoGALLERY;?>" data-fancybox="video" data-caption="<?php echo $gallery->titleGALLERY;?>">
+                                        <a href="<?php //echo $gallery->linkvideoGALLERY;?>" data-fancybox="video" data-caption="<?php //echo $gallery->titleGALLERY;?>">
                                             <div class="image">
-                                                <img src="<?php echo $thumbnail;?>" alt="<?php echo $gallery->titleGALLERY;?>">
+                                                <img src="<?php //echo $thumbnail;?>" alt="<?php //echo $gallery->titleGALLERY;?>">
                                                 <span class="play-button"><i class="mdi mdi-play"></i></span>
                                             </div>
-                                            <h4><?php echo $gallery->titleGALLERY;?></h4>
+                                            <h4><?php //echo $gallery->titleGALLERY;?></h4>
                                         </a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                        <?php } ?>
-                    <?php } ?>
+                        <?php //} ?>
+                    <?php //} ?> -->
+
+
+                    <div class="ui tab hero is-primary active" data-tab="nyat_kadir_gelar_buka_bersama_dengan_anak_jalanan">
+                        <div class="hero-body">
+                            <h1 class="title">Nyat Kadir gelar buka bersama dengan anak jalanan</h1>
+                            <div class="box">
+                                <a href="<?php echo base_url().$this->data['asfront'];?>img/slide1.jpg" data-fancybox="nyat_kadir_gelar_buka_bersama_dengan_anak_jalanan" data-caption="Nyat Kadir Gelar Buka Bersama">
+                                    <div class="image">
+                                        <img src="<?php echo base_url().$this->data['asfront'];?>img/slide1.jpg" alt="">
+                                    </div>
+                                    <h4>Perjalanan Menuju Pulau Setokok</h4>
+                                </a>
+                            </div>
+                            <div class="box">
+                                <a href="<?php echo base_url().$this->data['asfront'];?>img/sushi-man-edited.jpg" data-fancybox="nyat_kadir_gelar_buka_bersama_dengan_anak_jalanan" data-caption="Nyat Kadir Gelar Buka Bersama">
+                                    <div class="image">
+                                        <img src="<?php echo base_url().$this->data['asfront'];?>img/sushi-man-edited.jpg" alt="">
+                                    </div>
+                                    <h4>Sampai di Panti Pulau Setokok</h4>
+                                </a>
+                            </div>
+                            <div class="box">
+                                <a href="<?php echo base_url().$this->data['asfront'];?>img/campaign.jpg" data-fancybox="nyat_kadir_gelar_buka_bersama_dengan_anak_jalanan" data-caption="Nyat Kadir Gelar Buka Bersama">
+                                    <div class="image">
+                                        <img src="<?php echo base_url().$this->data['asfront'];?>img/campaign.jpg" alt="">
+                                    </div>
+                                    <h4>Bersalaman dengan Panti Pulau Setokok</h4>
+                                </a>
+                            </div>
+                            <div class="box">
+                                <a href="<?php echo base_url().$this->data['asfront'];?>img/nyatkadir1.jpg" data-fancybox="nyat_kadir_gelar_buka_bersama_dengan_anak_jalanan" data-caption="Nyat Kadir Gelar Buka Bersama">
+                                    <div class="image">
+                                        <img src="<?php echo base_url().$this->data['asfront'];?>img/nyatkadir1.jpg" alt="">
+                                    </div>
+                                    <h4>Beramah-tamah dengan Panti Pulau Setokok</h4>
+                                </a>
+                            </div>
+                            <div class="box">
+                                <a href="<?php echo base_url().$this->data['asfront'];?>img/nyatkadir2.jpg" data-fancybox="nyat_kadir_gelar_buka_bersama_dengan_anak_jalanan" data-caption="Nyat Kadir Gelar Buka Bersama">
+                                    <div class="image">
+                                        <img src="<?php echo base_url().$this->data['asfront'];?>img/nyatkadir2.jpg" alt="">
+                                    </div>
+                                    <h4>Beramah-tamah dengan Panti Pulau Setokok</h4>
+                                </a>
+                            </div>
+                        </div>
+                    </div> <!-- kelar Tab-Content 1 -->
+
+                    <div class="ui tab hero is-primary" data-tab="besar_besaran_kata">
+                        <div class="hero-body">
+                            <h1 class="title">Demi Bangsa dan Indonesia</h1>
+                            <div class="box">
+                                <a href="<?php echo base_url().$this->data['asfront'];?>img/campaign.jpg" data-fancybox="besar_besaran_kata" data-caption="Besar-besaran Kata">
+                                    <div class="image">
+                                        <img src="<?php echo base_url().$this->data['asfront'];?>img/campaign.jpg" alt="">
+                                    </div>
+                                    <h4>Perjalanan Menuju Panti Asuhan Aini</h4>
+                                </a>
+                            </div>
+                            <div class="box">
+                                <a href="<?php echo base_url().$this->data['asfront'];?>img/nyatkadir1.jpg" data-fancybox="besar_besaran_kata" data-caption="Besar-besaran Kata">
+                                    <div class="image">
+                                        <img src="<?php echo base_url().$this->data['asfront'];?>img/nyatkadir1.jpg" alt="">
+                                    </div>
+                                    <h4>Sampai di Panti Asuhan Aini</h4>
+                                </a>
+                            </div>
+                            <div class="box">
+                                <a href="<?php echo base_url().$this->data['asfront'];?>img/slide1.jpg" data-fancybox="besar_besaran_kata" data-caption="Besar-besaran Kata">
+                                    <div class="image">
+                                        <img src="<?php echo base_url().$this->data['asfront'];?>img/slide1.jpg" alt="">
+                                    </div>
+                                    <h4>Bersalaman dengan Panti Asuhan Aini</h4>
+                                </a>
+                            </div>
+                            <div class="box">
+                                <a href="<?php echo base_url().$this->data['asfront'];?>img/sushi-man-edited.jpg" data-fancybox="besar_besaran_kata" data-caption="Besar-besaran Kata">
+                                    <div class="image">
+                                        <img src="<?php echo base_url().$this->data['asfront'];?>img/sushi-man-edited.jpg" alt="">
+                                    </div>
+                                    <h4>Beramah-tamah dengan Panti Asuhan Aini</h4>
+                                </a>
+                            </div>
+                            <div class="box">
+                                <a href="<?php echo base_url().$this->data['asfront'];?>img/road-forest.jpg" data-fancybox="besar_besaran_kata" data-caption="Besar-besaran Kata">
+                                    <div class="image">
+                                        <img src="<?php echo base_url().$this->data['asfront'];?>img/road-forest.jpg" alt="">
+                                    </div>
+                                    <h4>Beramah-tamah dengan Panti Asuhan Aini</h4>
+                                </a>
+                            </div>
+                            <div class="box">
+                                <a href="<?php echo base_url().$this->data['asfront'];?>img/nyatkadir2.jpg" data-fancybox="besar_besaran_kata" data-caption="Besar-besaran Kata">
+                                    <div class="image">
+                                        <img src="<?php echo base_url().$this->data['asfront'];?>img/nyatkadir2.jpg" alt="">
+                                    </div>
+                                    <h4>Beramah-tamah dengan Panti Asuhan Aini</h4>
+                                </a>
+                            </div>
+                        </div>
+                    </div> <!-- kelar Tab-Content 1 -->
+
+
+
                 </div> <!-- kelar Gallery-Wrapper / Ancestor tile -->
             </div>
         </div> <!-- kelar div Columns -->
